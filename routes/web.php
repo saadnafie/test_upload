6f820611ea8b737986f7b345ba0699c140449607
Saad <saad@easy-trademarks.com>
2020-12-16 16:31:29 -0800

    new route added

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-
+Route::get('/test', 'HomeController@index')->name('test');
 
 
 //////////////////////shops/////////////////////////////////
